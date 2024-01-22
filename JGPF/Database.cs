3168c1da7b16313ea83f346729498aeb5db2a389
JGucwa <kubjaxxx@gmail.com>
2024-01-22 09:50:57 +0100

    Dodano Funckje Wyswietlania zawartosci bazy

--- JGPF/Database.cs
+++ JGPF/Database.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace JGPF
 {
-    class Database
+    public class Database
     {
+        readonly string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "baza.json");
+
+        public List<Zadanie> WyswietlWszystko()
+        {
+            try
+            {
+                string tekst = File.ReadAllText(dbPath);
+                return JsonConvert.DeserializeObject<List<Zadanie>>(tekst);
+            }
+            catch
+            {
+                return new List<Zadanie>();
+            }
+        }
+
     }
 }
